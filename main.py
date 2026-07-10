8bef94242b3471a402483281b099a000ebc92898
CesarAguirre9 <rpcesarmartin@gmail.com>
2026-07-10 15:16:21 -0500

    spaceshift moves correctly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
@@ -9,13 +10,26 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    dt = 0.0
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        for ob in updatable:
+            ob.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
